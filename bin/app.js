2217f51c338d2deedcdd3ff864497a387d56ac9c
Ilya Fakhrutdinov <bekamez@gmail.com>
2022-05-03 18:42:09 +0500

    build: fix executable file

--- bin/app.js
+++ bin/app.js
@@ -1,11 +1,16 @@
 #! /usr/bin/env node
 
-import getApp from '../dist/index.js';
-
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const index_1 = __importDefault(require('../dist/index'));
 const port = process.env.PORT || 5000;
 const address = '0.0.0.0';
-
-getApp({ port }).then((app) => {
+(0, index_1.default)().then((app) => {
   app.listen(port, address, () => {
     console.log(`Server has been started on ${port}`);
   });
