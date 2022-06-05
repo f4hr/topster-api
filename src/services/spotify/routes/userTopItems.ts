ecc9af866aab0e7d81c4221dd1d8069957923dfe
Ilya Fakhrutdinov <bekamez@gmail.com>
2022-06-05 14:50:37 +0500

    refactor(services): refactor Spotify service

--- src/services/spotify/routes/userTopItems.ts
+++ src/services/spotify/routes/userTopItems.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
+import routes, { TIME_RANGES } from '../utils';
+
 import type { ResponseError } from '../../../types';
+import type { TopItemsType, TimeRange } from '../utils';
 
-type TopItemsType = 'artists' | 'tracks';
-type TimeRange = 'long_term' | 'medium_term' | 'short_term';
-
-export type SpotifyTopItemsQuery = {
+type SpotifyTopItemsQuery = {
   [key: string]: any;
   accessToken: string;
   type?: TopItemsType;
@@ -14,26 +14,59 @@ export type SpotifyTopItemsQuery = {
   timeRange?: TimeRange;
 };
 
+const REQUIRED_FIELDS: string[] = [];
+
+const validateTopItems = (query: SpotifyTopItemsQuery) => {
+  const { timeRange, limit } = query;
+  const errors: ResponseError[] = [];
+
+  const missingFields = REQUIRED_FIELDS.filter((f) => !query[f]);
+
+  if (missingFields.length) {
+    errors.push({ message: `Missing required fields: "${missingFields.join(', ')}"` });
+  }
+
+  if (timeRange && !Object.values(TIME_RANGES).includes(timeRange)) {
+    errors.push({ message: `Invalid value "${timeRange}" for time range` });
+  }
+
+  if (limit && (limit < 0 || limit > 50)) {
+    errors.push({ message: 'Limit value out of range (0 < limit <= 50)' });
+  }
+
+  return errors;
+};
+
 const handler =
-  <R, D>(resourceType: TopItemsType, transformResponse: (data: R) => D) =>
+  <R, D>(
+    resourceType: TopItemsType,
+    transformResponse: (data: R) => D,
+    validate?: (query: SpotifyTopItemsQuery) => ResponseError[]
+  ) =>
   async (query: SpotifyTopItemsQuery): Promise<{ errors: ResponseError[]; data: D }> => {
     const response: { errors: ResponseError[]; data: any } = {
       errors: [],
       data: null,
     };
-    // const errors = validate(query);
+
+    // validate
+    const errors = validateTopItems(query);
+    if (validate) {
+      errors.push(...validate(query));
+    }
+    if (errors.length) response.errors = errors;
+
     const {
       access_token: accessToken,
       time_range: timeRange = 'medium_term',
       limit = 20,
       offset = 0,
     } = query;
-    // if (errors.length) response.errors = errors;
+
     const params = { time_range: timeRange, limit, offset };
 
-    // TODO: replace URL with function
     return axios
-      .get<R>(`https://api.spotify.com/v1/me/top/${resourceType}`, {
+      .get<R>(routes.topItems(resourceType), {
         params,
         headers: {
           Authorization: `Bearer ${accessToken}`,
--- src/services/spotify/utils.ts
+++ src/services/spotify/utils.ts
@@ -1,42 +1,28 @@
-// const API_URL = process.env.SPOTIFY_API_URL;
-// const PREFIX = '1';
+const API_URL = process.env.SPOTIFY_API_URL;
+const PREFIX = 'v1';
 
-// type ResourceType = 'artists' | 'tracks';
+export type TimeRange = 'short_term' | 'medium_term' | 'long_term';
+export type TopItemsType = 'artists' | 'tracks';
 
-// export const API_METHODS = {
-//   USER_INFO: 'user.getinfo',
-//   TOP_ALBUMS: 'user.gettopalbums',
-//   TOP_ARTISTS: 'user.gettopartists',
-// };
+export const TIME_RANGES: { [key: string]: TimeRange } = {
+  SHORT: 'short_term',
+  MEDIUM: 'medium_term',
+  LONG: 'long_term',
+};
 
-// export const authUrl = () => {
-//   const baseUrl = [AUTH_URL].join('/');
-//   const url = new URL(baseUrl);
+const buildApiUrl = (resource: string, params: { [key: string]: string } = {}) => {
+  const resourceUrl = [API_URL, PREFIX, resource].join('/');
+  const url = new URL(resourceUrl);
 
-//   url.searchParams.set('format', 'json');
-//   url.searchParams.set('api_key', API_KEY ?? '');
+  Object.keys(params).forEach((key) => {
+    url.searchParams.set(key, params[key]);
+  });
 
-//   Object.keys(params).forEach((key) => {
-//     url.searchParams.set(key, params[key]);
-//   });
+  return url.toString();
+};
 
-//   return url.toString();
-// };
+const routes = {
+  topItems: (resource: TopItemsType) => buildApiUrl(`me/top/${resource}`),
+};
 
-// export const buildApiUrl = (params: { [key: string]: string }) => {
-//   const baseUrl = [API_URL, PREFIX].join('/');
-//   const url = new URL(baseUrl);
-
-//   url.searchParams.set('format', 'json');
-//   url.searchParams.set('api_key', API_KEY ?? '');
-
-//   Object.keys(params).forEach((key) => {
-//     url.searchParams.set(key, params[key]);
-//   });
-
-//   return url.toString();
-// };
-
-// export default {
-//   buildApiUrl,
-// };
+export default routes;
